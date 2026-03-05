c0844db64de4c0c88cdf35050aa83585e476e465
Abhay Kamath <abhaykamath200714@gmail.com>
2026-03-05 18:17:46 +0530

    added login and register messages

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import regForm, loginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret'
 
 
 entries = [
@@ -33,3 +36,22 @@ def home():
 @app.route("/about")
 def about():
     return "<h1>I am Abhay</h1>"
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = regForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = loginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@financetracker.com' and form.password.data == 'admin':
+            flash("You have successfully logged in", 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Login unsuccessful", 'danger')
+    return render_template('login.html', title = 'Login', form = form)
