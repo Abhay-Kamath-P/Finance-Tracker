464b41450bab080163ef7fa216af95aac14d6933
Abhay Kamath <abhaykamath200714@gmail.com>
2026-03-09 19:25:51 +0530

    Added SQLAlchemy database

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import regForm, loginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(24), nullable=False)
+    entries = db.relationship('Entry', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    #type should be either 'income' or 'expense'
+    type = db.Column(db.String(10), nullable=False)
+    category = db.Column(db.String(20), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Entry('{self.debit}', '{self.credit}', '{self.category}', '{self.date}')"
+    
+with app.app_context():
+    db.create_all()
 
 entries = [
     {
@@ -50,8 +79,8 @@ def login():
     form = loginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@financetracker.com' and form.password.data == 'admin':
-            flash("You have successfully logged in", 'success')
+            flash("You have successfully logged in!", 'success')
             return redirect(url_for('home'))
         else:
-            flash("Login unsuccessful", 'danger')
+            flash("Login unsuccessful, check your email or password.", 'danger')
     return render_template('login.html', title = 'Login', form = form)
