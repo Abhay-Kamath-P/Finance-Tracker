320a99fff5008fec3f8ea84b25506a82a56e10da
Abhay Kamath <abhaykamath200714@gmail.com>
2026-03-11 21:01:34 +0530

    Added user authentication

--- finance_tracker/__init__.py
+++ finance_tracker/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+from finance_tracker import routes
 
 with app.app_context():
     db.create_all()
-
-from finance_tracker import routes
